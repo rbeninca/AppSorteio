df91e215d93fa9ba5129f545ab798eaf74a0110c
rbeninca <rbeninca@gmail.com>
2020-02-27 11:22:48 -0300

    sorteio funcionando

--- app/src/main/java/br/ifsc/edu/sorteio/MainActivity.java
+++ app/src/main/java/br/ifsc/edu/sorteio/MainActivity.java
@@ -3,6 +3,12 @@ package br.ifsc.edu.sorteio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void sorteiaNumero(View view){
+        TextView textView = findViewById(R.id.textView);
+        EditText editTextIncial = findViewById(R.id.editTextInicial);
+        EditText editTextFinal = findViewById(R.id.editTextFinal);
+
+        int valorInicial= Integer.parseInt( editTextIncial.getText().toString());
+        int valorFinal = Integer.parseInt(editTextFinal.getText().toString());
+        int numeroSorteado=  new Random().nextInt(valorFinal-valorInicial)+valorInicial;
+
+
+
+        textView.setText(Integer.toString(numeroSorteado));
+
     }
 }
